fb768c6a408e6bea49e0289d0011bfe80f922b7e
Pouria Rouzrokh <rouzrokh.pouria@mayo.edu>
2023-10-30 00:14:42 -0500

    added empty prompts.py file

---
+++ radqg/prompts.py
@@ -0,0 +1,43 @@
+##########################################################################################
+# Description: A script containing prompts for generating questions with the LLM.
+##########################################################################################
+
+# ----------------------------------------------------------------------------------------
+# get_qa_prompt
+
+def get_qa_prompt(
+    figure_caption: str,
+    context: str,
+    type_of_question: str
+) -> str:
+    
+    prompt = f"""
+    """
+    
+    return prompt 
+
+# ----------------------------------------------------------------------------------------
+# edit_qa_prompt
+
+def edit_qa_prompt(
+    figure_caption: str,
+    qa_dict_string: str
+) -> str:
+    
+    prompt = f"""
+    """
+    
+    return prompt 
+
+
+# ----------------------------------------------------------------------------------------
+# get_dict_formatting_prompt
+
+def get_dict_formatting_prompt(
+    qa_dict_string: str
+) -> str:
+    
+    prompt = f"""
+    """
+    
+    return prompt 
